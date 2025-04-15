d2046abb5f5350d8f32da1ce53b9bd8902e8504d
adgfratec43 <133554359+adfgdartec@users.noreply.github.com>
2025-04-15 10:06:52 -0500

    Create Navigation.js

---
+++ src/Comps/Header/Navigation.js
@@ -0,0 +1,20 @@
+
+const Navigation = () => {
+  return (
+    <nav>
+         <div className="container">
+             <div className="row">
+                  <div className="col-sm-3 navbarPart1">
+                      <Button className="allCategoriesTab">
+                          <span class="text">ALL CATEGORIES</span>
+                        </Button>
+                  </div>
+
+                  <div className="col-sm-9 navbarPart2">
+                        
+                  </div>
+            </div>
+      </div>
+  </nav>
+  )
+}
